2c1e3c3ee4cdbc6a6ad140b9591830967a9aafa7
zhijie032 <1752983361@qq.com>
2019-11-26 10:34:15 +0800

    ;;

--- route/route.php
+++ route/route.php
@@ -31,6 +31,7 @@ Route::group('', function(){ /* 检测登录  这里的控制器的访问方法�
 
 Route::resource('v1/houses','Admin/Api');
 Route::get('v1/cates','Admin/Api/cates');
+
 Route::get('v1/searchs','Admin/Api/searchs');
 
 
