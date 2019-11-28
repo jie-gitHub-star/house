aab2aa55cd46712af069513b24592a27cd33c7af
zhijie032 <56252557+jie-gitHub-star@users.noreply.github.com>
2019-11-28 15:28:41 +0800

    updateapi.php

--- 应用程序/管理员/控制器/Api.php
+++ 应用程序/管理员/控制器/Api.php
@@ -9,8 +9,8 @@ use app\Admin\model\Admin;
 
 class Api extends Controller
 {
-    private $appid='wx581d3e61cb49a511';
-    private $secret='bdc820c9cd42b5330d4599b891ebc79c';
+    private $appid='';
+    private $secret='';
 
     /**
      * 显示资源列表
